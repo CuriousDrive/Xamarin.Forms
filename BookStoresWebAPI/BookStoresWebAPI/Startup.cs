c8588a5686f61cc554f9f8302737bfb42382b171
curious.drive <curious.drive@gmail.com>
2019-11-24 22:05:11 -0500

    Entity Framework Core : REST API Methods - Part 3

--- BookStoresWebAPI/BookStoresWebAPI/Startup.cs
+++ BookStoresWebAPI/BookStoresWebAPI/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStoresWebAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,9 @@ namespace BookStoresWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<BookStoresDBContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("BookStoresDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
